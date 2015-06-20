9678536e988d7c9412cd34c53ac077aa45a14caf
RJ Robinson <robertj.robinson@gmail.com>
2015-06-20 15:28:05 -0400

    added gulp. restructured files.

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    open = require('gulp-open'),
+    browserify = require('gulp-browserfiy'),
+    concat = require('gulp-concat'),
+    port = process.env.port || 3031;
