03b4c3df6a1acde4dce16b9bc4afd2624c7d0262
RJ Robinson <robertj.robinson@gmail.com>
2015-06-22 09:37:14 -0400

    added it

--- app/dist/js/.module-cache/cce84e0deb05150ce10e5f94879acd11e3368770.js
+++
@@ -1,8 +0,0 @@
-/** @jsx React.DOM */
-var React = require('react'),
-    Feed = require('./components/Feed');
-
-React.renderComponent(
-    React.createElement(Feed, null),
-    document.getElementById('app')
-);
